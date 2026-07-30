bc9f9f42069ccf32361e4b4ad4a560c682f1d416
HaleyJ4 <pnfgnfmzd2@privaterelay.appleid.com>
2026-07-29 23:37:16 -0300

    updates

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,5 +1,8 @@
-const nextConfig = {
-  reactStrictMode: true,
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+allowedDevOrigins: ["192.168.1.53"],
 };
 
 export default nextConfig;
+
